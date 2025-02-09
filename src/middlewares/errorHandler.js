6d4c9008acebd782125ac8d394354f77ff6f6a17
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-09 19:22:51 +0200

    error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,7 @@
 
 //Імпортуємо клас HttpError для обробки помилок HTTP
 import { HttpError } from 'http-errors'; //клас HttpError для обробки помилок HTTP
+import { Mongoose } from 'mongoose';
 
 export const errorHandler = (err, req, res, next) => {
   // Перевірка, чи отримали ми помилку від createHttpError (404)
@@ -12,6 +13,26 @@ export const errorHandler = (err, req, res, next) => {
       data: err,
     });
     return;
+  }
+
+  if (err instanceof Mongoose) {
+    return res.status(500).json({
+      status: 500,
+      message: err.message,
+      name: 'Mongoose error',
+    });
+  }
+
+  if (err.isJoi) {
+    return res.status(400).json({
+      status: 400,
+      message: err.message,
+      errors: err.details.map((err) => ({
+        message: err.message,
+        path: err.path,
+      })),
+      name: 'Validation error',
+    });
   }
 
   res.status(500).json({
